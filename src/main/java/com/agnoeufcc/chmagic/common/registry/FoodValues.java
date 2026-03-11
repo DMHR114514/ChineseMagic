ce8c72a46b10ea378fbd0aa9c3f398d587a92245
ExTetragon <denglehua1998@outlook.com>
2026-03-11 17:53:09 +0800

    食物

--- src/main/java/com/agnoeufcc/chmagic/common/registry/FoodValues.java
+++ src/main/java/com/agnoeufcc/chmagic/common/registry/FoodValues.java
@@ -1,72 +1,58 @@
-package com.agnoeufcc.chmagic.common;
+package com.agnoeufcc.chmagic.common.registry;
 
-import com.google.common.collect.ImmutableMap;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 //import com.agnoeufcc.chmagic.common.registry.ModEffects;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public class FoodValues
 {
     // basic foods
-    public static final FoodProperties DADOU = (new FoodProperties.Builder())
+    public static final FoodProperties SOYBEAN = new FoodProperties.Builder()
             .nutrition(2)
             .saturationModifier(0.5f)
             .fast()
             .build();
-    public static final FoodProperties MAODOU = (new FoodProperties.Builder())
+    public static final FoodProperties GREEN_SOYBEAN = new FoodProperties.Builder()
             .nutrition(3)
             .saturationModifier(0.5f)
             .fast()
             .build();
-    public static final FoodProperties SOYAMILK_BOTTLE = (new FoodProperties.Builder())
-            .nutrition(5)
-            .saturationModifier(0.5f)
-            .build();
-    public static final FoodProperties SALTY_TOFU_CURD= (new FoodProperties.Builder())
+    public static final FoodProperties SALTY_TOFU_CURD= new FoodProperties.Builder()
             .nutrition(10)
             .saturationModifier(1.0f)
             .usingConvertsTo(Items.BOWL)
             .build();
-    public static final FoodProperties SWEET_TOFU_CURD= (new FoodProperties.Builder())
+    public static final FoodProperties SWEET_TOFU_CURD= new FoodProperties.Builder()
             .nutrition(10)
             .saturationModifier(1.0f)
             .usingConvertsTo(Items.BOWL)
             .build();
-    public static final FoodProperties TOFU= (new FoodProperties.Builder())
+    public static final FoodProperties TOFU= new FoodProperties.Builder()
             .nutrition(4)
             .saturationModifier(0.5f)
             .fast()
             .build();
-    public static final FoodProperties FROZEN_TOFU = (new FoodProperties.Builder())
+    public static final FoodProperties FROZEN_TOFU = new FoodProperties.Builder()
             .nutrition(4)
             .saturationModifier(0.5f)
-            .fast()
             .build();
-    public static final FoodProperties MOULD_TOFU = (new FoodProperties.Builder())
+    public static final FoodProperties HAIRY_TOFU = new FoodProperties.Builder()
             .nutrition(5)
             .saturationModifier(0.5f)
-            .fast()
             .build();
-    public static final FoodProperties STINKY_TOFU= (new FoodProperties.Builder())
+    public static final FoodProperties STINKY_TOFU= new FoodProperties.Builder()
             .nutrition(8)
             .saturationModifier(0.625f)
             .build();
-    public static final FoodProperties BAKED_TOFU= (new FoodProperties.Builder())
+    public static final FoodProperties BAKED_TOFU= new FoodProperties.Builder()
             .nutrition(8)
             .saturationModifier(1.0f)
             .build();
-    public static final FoodProperties FURU= (new FoodProperties.Builder())
+    public static final FoodProperties FERMENTED_TOFU= new FoodProperties.Builder()
             .nutrition(5)
             .saturationModifier(0.5f)
             .build();
-    public static final FoodProperties DOUPI= (new FoodProperties.Builder())
+    public static final FoodProperties TOFU_SKIN= (new FoodProperties.Builder())
             .nutrition(6)
             .saturationModifier(0.5f)
             .fast()
@@ -74,6 +60,21 @@ public class FoodValues
     public static final FoodProperties QIANZHANG_TOFU= (new FoodProperties.Builder())
             .nutrition(5)
             .saturationModifier(0.5f)
-            .fast()
             .build();
+    public static final FoodProperties TOFU_JERKY= (new FoodProperties.Builder())
+            .nutrition(5)
+            .saturationModifier(0.5f)
+            .build();
+    public static final FoodProperties LATIAO= (new FoodProperties.Builder())
+            .nutrition(3)
+            .saturationModifier(0.5f)
+            .build();
+
+/*
+    public static final FoodProperties SOYAMILK_BOTTLE = (new FoodProperties.Builder())
+            .nutrition(5)
+            .saturationModifier(0.5f)
+            .build();
+ */
+
 }
--- src/main/java/com/agnoeufcc/chmagic/common/registry/ModItems.java
+++ src/main/java/com/agnoeufcc/chmagic/common/registry/ModItems.java
@@ -1,7 +1,9 @@
 package com.agnoeufcc.chmagic.common.registry;
 
 import com.agnoeufcc.chmagic.ChineseMagic;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -10,10 +12,25 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS =
             DeferredRegister.createItems(ChineseMagic.MOD_ID);
 
+    //Helper Methods
+    public static Item.Properties foodItem(FoodProperties food) {
+        return new Item.Properties().food(food);
+    }
+
+    public static Item.Properties bowlFoodItem(FoodProperties food) {
+        return new Item.Properties().food(food).craftRemainder(Items.BOWL).stacksTo(16);
+    }
+
+    public static Item.Properties drinkItem() {
+        return new Item.Properties().craftRemainder(Items.GLASS_BOTTLE).stacksTo(16);
+    }
+
+
+    //Beans
     public static final DeferredItem<Item> SOYBEAN =
-            ITEMS.register("soybean", ()-> new Item(new Item.Properties()));
+            ITEMS.register("soybean", ()-> new Item(foodItem(FoodValues.SOYBEAN)));
     public static final DeferredItem<Item> GREEN_SOYBEAN =
-            ITEMS.register("green_soybean", ()-> new Item(new Item.Properties()));
+            ITEMS.register("green_soybean", ()-> new Item(foodItem(FoodValues.GREEN_SOYBEAN)));
     public static final DeferredItem<Item> POD =
             ITEMS.register("pod", ()-> new Item(new Item.Properties()));
     public static final DeferredItem<Item> DRIED_POD =
@@ -21,31 +38,31 @@ public class ModItems {
     public static final DeferredItem<Item> SOYBEAN_POWDER =
             ITEMS.register("soybean_powder", ()-> new Item(new Item.Properties()));
     public static final DeferredItem<Item> TOFU =
-            ITEMS.register("tofu", ()-> new Item(new Item.Properties()));
+            ITEMS.register("tofu", ()-> new Item(foodItem(FoodValues.TOFU)));
     public static final DeferredItem<Item> FROZEN_TOFU =
-            ITEMS.register("frozen_tofu", ()-> new Item(new Item.Properties()));
+            ITEMS.register("frozen_tofu", ()-> new Item(foodItem(FoodValues.FROZEN_TOFU)));
     public static final DeferredItem<Item> HAIRY_TOFU =
-            ITEMS.register("hairy_tofu", ()-> new Item(new Item.Properties()));
+            ITEMS.register("hairy_tofu", ()-> new Item(foodItem(FoodValues.HAIRY_TOFU)));
     public static final DeferredItem<Item> STINKY_TOFU =
-            ITEMS.register("stinky_tofu", ()-> new Item(new Item.Properties()));
+            ITEMS.register("stinky_tofu", ()-> new Item(foodItem(FoodValues.STINKY_TOFU)));
     public static final DeferredItem<Item> SALTY_TOFU_CURD =
-            ITEMS.register("salty_tofu_curd", ()-> new Item(new Item.Properties()));
+            ITEMS.register("salty_tofu_curd", ()-> new Item(bowlFoodItem(FoodValues.SALTY_TOFU_CURD)));
     public static final DeferredItem<Item> SWEET_TOFU_CURD =
-            ITEMS.register("sweet_tofu_curd", ()-> new Item(new Item.Properties()));
+            ITEMS.register("sweet_tofu_curd", ()-> new Item(bowlFoodItem(FoodValues.SWEET_TOFU_CURD)));
     public static final DeferredItem<Item> BAKED_TOFU =
-            ITEMS.register("baked_tofu", ()-> new Item(new Item.Properties()));
+            ITEMS.register("baked_tofu", ()-> new Item(foodItem(FoodValues.BAKED_TOFU)));
     public static final DeferredItem<Item> FERMENTED_TOFU =
-            ITEMS.register("fermented_tofu", ()-> new Item(new Item.Properties()));
+            ITEMS.register("fermented_tofu", ()-> new Item(foodItem(FoodValues.FERMENTED_TOFU)));
     public static final DeferredItem<Item> TOFU_SKIN =
-            ITEMS.register("tofu_skin", ()-> new Item(new Item.Properties()));
+            ITEMS.register("tofu_skin", ()-> new Item(foodItem(FoodValues.TOFU_SKIN)));
     public static final DeferredItem<Item> QIANZHANG_TOFU =
-            ITEMS.register("qianzhang_tofu", ()-> new Item(new Item.Properties()));
+            ITEMS.register("qianzhang_tofu", ()-> new Item(foodItem(FoodValues.QIANZHANG_TOFU)));
     public static final DeferredItem<Item> SOYBEAN_MEAL =
             ITEMS.register("soybean_meal", ()-> new Item(new Item.Properties()));
     public static final DeferredItem<Item> TOFU_JERKY =
-            ITEMS.register("tofu_jerky", ()-> new Item(new Item.Properties()));
+            ITEMS.register("tofu_jerky", ()-> new Item(foodItem(FoodValues.TOFU_JERKY)));
     public static final DeferredItem<Item> LATIAO =
-            ITEMS.register("latiao", ()-> new Item(new Item.Properties()));
+            ITEMS.register("latiao", ()-> new Item(foodItem(FoodValues.LATIAO)));
 
     public static final DeferredItem<Item> BLACKBEAN =
             ITEMS.register("blackbean", ()-> new Item(new Item.Properties()));
@@ -60,6 +77,7 @@ public class ModItems {
 
     public static final DeferredItem<Item> SALT =
             ITEMS.register("salt", ()-> new Item(new Item.Properties()));
+
 
 
     public static void register(IEventBus eventBus){
